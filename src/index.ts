5ca6cff58073a03b129f0c99f77b2a57a925275c
yuri bueno <yuri.bueno@live.com>
2024-06-16 05:04:52 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import { appCore } from "base-api-y";
+
+new appCore({ port: 3000, cors: {} }).init();
---
+++ src/routes/exemplo.ts
@@ -0,0 +1,3 @@
+import { Router } from "base-api-y";
+
+new Router({ path: "cu55555", method: "get", execute(req, res) {} });
