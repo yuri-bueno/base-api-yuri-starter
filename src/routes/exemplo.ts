a3887bf3672f6bfed1de6a6ef45e25848a335512
yuri bueno <yuri.bueno@live.com>
2024-06-27 01:35:37 -0300

    1.1.5

--- src/routes/exemplo.ts
+++ src/routes/exemplo.ts
@@ -1,9 +1,34 @@
-import { Router } from "zac-api";
+import { Route, apiErrors } from "zac-api";
 
-new Router({
-  path: "/user",
-  method: "get",
+import z from "zod";
+
+new Route({
+  method: "post",
+  path: "/exemplo",
+  files: { folder: "test", type: "image/", max: 2 },
+  params: {
+    body: z.object({
+      name: z.string(),
+      isTrue: z.coerce.boolean().default(true),
+      idade: z.coerce.number().max(5, apiErrors.LONG_NUMBER_ERROR),
+      email: z.string().email(apiErrors.INVALID_TEXT_REGEX_ERROR),
+      list: z.array(z.string().max(5)).or(
+        //or in muilti-form = (files exist)
+        z
+          .string()
+          .max(5)
+          .transform((field) => [field])
+      ),
+    }),
+  },
+
   execute(req, res) {
-    req.body;
+    const files = req.saveFiles();
+
+    if (files.success) {
+      console.log(files.ids);
+    }
+
+    res.status(200).json({ body: req.body });
   },
 });
